98593ff160320842c71139c002eaa60d0aa54cbd
mohich <mohich@abv.bg>
2021-12-31 16:41:14 +0200

    Add files via upload

---
+++ Task 3. Cargo 1.hs
@@ -0,0 +1,24 @@
+ isPossibleToCarry1 x y =
+     if x<=y
+     then "It is possible to carry"
+     else "The load is too large"
+
+ main = do
+   input <- getLine
+   let tankerVolume = read input :: Float
+
+   input1 <- getLine
+   let diameters = read input1 :: [Float]
+
+   input2 <- getLine
+   let lenghts = read input2 :: [Float]
+
+   input3 <- getLine
+   let tankCounts = read input3 :: [Float]
+
+   let volumes = zipWith(\ d l -> pi / 4 * d * d * l) diameters lenghts
+
+   print(volumes) -- volumes
+   let totalVolumes = zipWith(\ a tC -> a * tC) volumes tankCounts
+   let totalVolume = foldl (+) 0 totalVolumes
+   print(totalVolume)
